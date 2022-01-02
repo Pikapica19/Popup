d9f2945b0b175554965ad0634535ccc17ecdd1f4
Priyanka Shirsat <67407614+Pikapica19@users.noreply.github.com>
2022-01-02 14:45:07 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,26 @@
+const btn = document.querySelector("#btn");
+const popup = document.querySelector(".popup");
+const close = document.querySelector(".close");
+
+btn.addEventListener("click", openModel);
+popup.addEventListener("click", closeModel);
+close.addEventListener("click", closeModel);
+
+popup.addEventListener("click", (e) => {
+  const evt = e.target.classList;
+  if (evt.contains("popup") || evt.contains("close")) {
+    closeModal();
+  }
+});
+
+function openModel(e){
+  e.preventDefault();
+  popup.style.display = "block";
+}
+
+function closeModel(){
+  popup.style.display = "none";
+}
+
+
+
